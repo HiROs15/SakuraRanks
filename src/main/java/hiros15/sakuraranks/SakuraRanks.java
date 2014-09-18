56f7ea47bf28ef2a84442113577d1a80ead30c0c
Kyle <Kyle@192.168.1.147>
2014-09-18 17:43:08 -0400

    v1.0

---
+++ src/main/java/hiros15/sakuraranks/SakuraRanks.java
@@ -0,0 +1,15 @@
+package hiros15.sakuraranks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SakuraRanks extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
